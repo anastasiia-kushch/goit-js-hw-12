3be7fe14ddb68821bb35ae31ed8ebb57d3d9b2fc
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-01-19 13:58:22 +0100

    added axios

--- src/js/searchImages.js
+++ src/js/searchImages.js
@@ -1,25 +1,15 @@
-import { BASE_URL, API_KEY } from './refs';
-import iziToast from 'izitoast';
+import axios from 'axios';
 
-export const searchImages = query => {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
-
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Response is not success: ${response.status}`);
-      }
-
-      return response.json();
-    })
-    .then(data => {
-      if (data.total === 0) {
-        iziToast.info({
-          position: 'center',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      }
-      return data;
-    });
+export const getPhotos = (query, page) => {
+  axios.defaults.baseURL = 'https://api.unsplash.com';
+  axios.defaults.headers.common['Authorization'] =
+    'Client-ID LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc';
+  return axios.get('/search/photos', {
+    params: {
+      query,
+      page,
+      per_page: 40,
+      orientation: 'portrait',
+    },
+  });
 };
